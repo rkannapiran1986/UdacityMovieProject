506a4a6b7aee009c3d1503ba0291899550893c02
Kannapiran R <kannapiran.r@ge.com>
2017-11-11 17:21:30 +0530

    Review changes

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,40 +5,48 @@ import fresh_tomatoes
 # Here is the list of movies describe the instance of Movie and TvShow Classes
 avatar = media.Movie("Avatar",
                      "new movie",
-                     """https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg""",
+                     "https://upload.wikimedia.org/wikipedia/"
+                     "en/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY",
                      "2h 42m")
 
 ironMan = media.Movie('Iron Man 3',
-                      'new movie for iron man',
-                      'http://www.swiftfilm.com/wp-content/uploads/2013/01/IRON3_BusShelter_Falling_v7.jpg',
-                      'https://www.youtube.com/watch?v=Ke1Y3P9D0Bc',
-                      '2h 10m')
+                      "New movie for iron man",
+                      "http://www.swiftfilm.com/wp-content/uploads/"
+                      "2013/01/IRON3_BusShelter_Falling_v7.jpg",
+                      "https://www.youtube.com/watch?v=Ke1Y3P9D0Bc",
+                      "2h 10m")
 
-antMan = media.Movie('Ant-Man',
-                     'Man becoming a super hero',
-                     'https://pre11.deviantart.net/205c/th/pre/i/2015/129/9/d/ant_man_poster_by_gingerjmez-d8spj3h.jpg',
-                     'https://www.youtube.com/watch?v=pWdKf3MneyI',
-                     '1h 58m')
+antMan = media.Movie("Ant-Man",
+                     "Man becoming a super hero",
+                     "https://pre11.deviantart.net/205c/"
+                     "th/pre/i/2015/129/9/d/"
+                     "ant_man_poster_by_gingerjmez-d8spj3h.jpg",
+                     "https://www.youtube.com/watch?v=pWdKf3MneyI",
+                     "1h 58m")
 
-spiderMan = media.Movie('Spider Man',
-                        'Man who do super duper stunts..',
-                        'https://lumiere-a.akamaihd.net/v1/images/image_ccc4b657.jpeg',
-                        'https://www.youtube.com/watch?v=rk-dF1lIbIg',
-                        '2h 13m')
+spiderMan = media.Movie("Spider Man",
+                        "Man who do super stunts..",
+                        "https://lumiere-a.akamaihd.net/v1"
+                        "/images/image_ccc4b657.jpeg",
+                        "https://www.youtube.com/watch?v=rk-dF1lIbIg",
+                        "2h 13m")
 
-threeIdiots = media.Movie('3 Idiots',
-                          'Indian Movie about three friends',
-                          'https://sc-events.s3.amazonaws.com/4224477/main.jpg',
-                          'https://www.youtube.com/watch?v=xvszmNXdM4w',
-                          '2h 51m')
+threeIdiots = media.Movie("3 Idiots",
+                          "Indian Movie about three friends",
+                          "https://sc-events.s3.amazonaws.com/"
+                          "4224477/main.jpg",
+                          "https://www.youtube.com/watch?v=xvszmNXdM4w",
+                          "2h 51m")
 
 big_bang_theory = media.TvShow('The Big Bang Theory',
                                'Season 10',
-                               'http://www.gstatic.com/tv/thumb/tvbanners/14215412/p14215412_b_v8_ad.jpg',
+                               "http://www.gstatic.com/tv/thumb/"
+                               "tvbanners/14215412/p14215412_b_v8_ad.jpg",
                                'https://www.youtube.com/watch?v=WBb3fojgW0Q',
                                'September 19, 2016 - May 11, 2017')
 
+# Assigning all values in an array
 movies = [avatar,
           ironMan,
           antMan,
@@ -46,5 +54,5 @@ movies = [avatar,
           threeIdiots,
           big_bang_theory]
 
-
+# Calling the functon to open webbrowser to display the result
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -2,15 +2,19 @@ import webbrowser
 
 
 class Video():
+    """ this is commonly shared class """
     def __init__(self, title, duration):
         self.title = title
         self.duration = duration
 
 
 class Movie(Video):
-    """Document for Movie class"""
+    """Document for Movie class.
+
+        This will have entire details about Movie
+    """
     def __init__(self, movie_title, sline, p_img, t_youtube, duration):
-        """Calling Video Call to get Title and duration"""
+        """ Calling Video Call to get Title and duration """
         mDetail = Video(movie_title, duration)
         self.title = mDetail.title
         self.storyline = sline
@@ -19,11 +23,15 @@ class Movie(Video):
         self.duration = mDetail.duration
 
     def showTrailer(self):
+        """ Function for calling show trailer (single movie) """
         webbrowser.open(self.trailer_url)
 
 
 class TvShow(Video):
-    """Document for TvShow class"""
+    """Document for TvShow class.
+
+        This will have entire details about TV serials
+    """
     def __init__(self, season_title, season, episode, tv_station, duration):
         """Calling Video Call to get Title and duration"""
         mDetail = Video(season_title, duration)
